7a28190634c6fa719e362ec810d55ee084b186c5
Shwetalbhargav <hridaan.purav@gmail.com>
2024-12-08 15:48:19 +0530

    fixed db

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const errorHandler = require('./errorHandler');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true, 
+}
+));
 app.use(express.json());
 
 
